0ab28bd890cf9e73fb929b92a301ae28b7390c59
hoblin <evgeny.gurin@gmail.com>
2018-04-25 23:32:13 +0300

    Updating files instead of overwriting

--- lib/screeps.rb
+++ lib/screeps.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'digest'
 require 'awesome_print'
 require_relative 'client.rb'
 
@@ -13,17 +14,28 @@ class Screeps
   def download
     response = client.download
     branch = response['branch']
-    response['modules'].each_pair do |ident, body|
+    response['modules'].each_pair do |identifier, body|
       FileUtils.mkdir_p File.join('js', branch)
-      # TODO: Update if file was changed only
-      # TODO: Move to separate method and add js2coffee compilling
-      File.open(File.join('js', branch, ident + '.js'), 'w') do |f|
-        f << body
-      end
+      file_name = File.join('js', branch, identifier + '.js')
+      update_local_file_if_needed(file_name, body)
     end
   end
 
   def upload
     ap client.upload
+  end
+
+  private
+
+  def update_local_file_if_needed(file_name, content)
+    existing_file_data = File.read(file_name)
+    save_file(file_name, content) unless Digest::MD5.hexdigest(existing_file_data) == Digest::MD5.hexdigest(content)
+    # TODO: Add js2coffee compilling
+  end
+
+  def save_file(file_name, content)
+    File.open(file_name, 'w') do |f|
+      f << content
+    end
   end
 end
